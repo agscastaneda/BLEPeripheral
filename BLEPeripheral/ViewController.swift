c740328726f2bae6f8b3aa60dcd9e91ccb78c182
Agustin Castañeda <ags.castaneda@gmail.com>
2020-08-18 16:51:14 -0500

    Initial Commit

---
+++ BLEPeripheral/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BLEPeripheral
+//
+//  Created by Agustin Castaneda on 18/08/20.
+//  Copyright © 2020 Agustin Castaneda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
